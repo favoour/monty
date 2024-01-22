4a575f52b20f883d035e08f5d7f8f2e0b5f20511
favoour <favour.fisayo@yahoo.com>
2024-01-21 18:48:37 -0800

    newww

--- 0-add.c
+++ 0-add.c
@@ -1,70 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "monty.h"
+/**
+ * add_stack - adds the top two elements of the stack.
+ * @head: The stack head
+ * @line_number: line_number
+ * Return: Always return nothing
+*/
+void add_stack(stack_t **head, unsigned int line_number)
+{
+stack_t *h;
+int len = 0, result;
 
-typedef struct StackNode {
-    int n;
-    struct StackNode *next;
-} stack_t;
-
-void add_stack(stack_t **head, unsigned int line_number);
-void free_stack(stack_t *head);
-
-struct {
-    FILE *file;
-    char *content;
-} interpreter;
-
-void free_stack(stack_t *head) {
-    while (head) {
-        stack_t *temp = head;
-        head = head->next;
-        free(temp);
-    }
+h = *head;
+while (h)
+{
+h = h->next;
+len++;
 }
-
-int main() {
-    stack_t *stack = NULL;
-
-    add_stack(&stack, 1);
-    add_stack(&stack, 2);
-    add_stack(&stack, 3);
-
-    stack_t *current = stack;
-    printf("Stack values:");
-    while (current) {
-        printf(" %d", current->n);
-        current = current->next;
-    }
-    printf("\n");
-
-    free_stack(stack);
-
-    return 0;
+if (len < 2)
+{
+fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+fclose(interpreter.file);
+free(interpreter.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
 }
-
-void add_stack(stack_t **head, unsigned int line_number) {
-    stack_t *h;
-    int len = 0, result;
-
-    h = *head;
-
-    while (h) {
-        h = h->next;
-        len++;
-    }
-
-    if (len < 2) {
-        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-        fclose(interpreter.file);
-        free(interpreter.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-
-    h = *head;
-    result = h->n + h->next->n;
-    h->next->n = result;
-    *head = h->next;
-    free(h);
+h = *head;
+result = h->n + h->next->n;
+h->next->n = result;
+*head = h->next;
+free(h);
 }
-
